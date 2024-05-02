94d7e90d7c235a827a715c4cbecb603adc67a89e
Justus <galaxycheesemc@gmail.com>
2024-05-02 16:38:35 -0700

    Fixed format

--- scripts/temple.js
+++ scripts/temple.js
@@ -1,8 +1,26 @@
 const mainnav = document.querySelector('.navigation')
 const hambutton = document.querySelector('#menu');
 
-// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
 hambutton.addEventListener('click', () => {
 	mainnav.classList.toggle('show');
 	hambutton.classList.toggle('show');
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const navigationLinks = document.querySelectorAll('.navigation a');
+
+    navigationLinks.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); // Prevent the default link behavior
+            
+            navigationLinks.forEach(function(link) {
+                link.classList.remove('active');
+            });
+            
+            link.classList.add('active');
+
+            const mainTitle = document.querySelector('main h2');
+            mainTitle.textContent = link.getAttribute('title');
+        });
+    });
 });
